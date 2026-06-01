baa4772f7a4f57c53e3a2d4a1b33bc32ebe9023d
Nishanta Baral <43251023+NishantaBaral@users.noreply.github.com>
2026-06-01 14:01:54 -0700

    Update Inital grid of particles.py

--- Inital grid of particles.py
+++ Inital grid of particles.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-output = open('/Users/nishantabaral/Desktop/Geophysical Flows/Double Gyre Flow/Trace Analysis/Inital grid of particles.txt','w')
-X,Y = np.meshgrid(np.linspace(0,2,5000),np.linspace(0,1,2500))
+output = open('/Users/nishantabaral/Desktop/Geophysical Flows/Double Gyre Flow/Inital grid of particles.txt','w')
+X,Y = np.meshgrid(np.linspace(0,2,500),np.linspace(0,1,250))
 positions = np.column_stack([X.ravel(),Y.ravel()])
 np.savetxt(output,positions)
 output.close()
